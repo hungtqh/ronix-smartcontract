d1c0bd8b874ab534f581e46b20927b8ed962b859
Quang Hung <tech.hungtq@gmail.com>
2024-04-26 17:06:36 +0700

    Add erc721arb

---
+++ contracts/C168NFTArbitrum.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// Lock on Ethereum, mint on Arbitrum
+
+contract MyToken is ERC721, ERC721Burnable, Ownable {
+    constructor(address initialOwner)
+        ERC721("MyToken", "MTK")
+        Ownable(initialOwner)
+    {}
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+}
